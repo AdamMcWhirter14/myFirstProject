224dd9765203d7c7ab86134932f19a65d5b8f916
Adam McWhirter <adammcwhirterwork@gmail.com>
2026-03-16 15:46:25 +0000

    Added CRUD - create, edit, update and delete jobs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,14 +10,16 @@
 
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4">
-            <div class="flex h-16 items-center justify-between">
-                <div class="flex items-center space-x-4">
-                    <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                    <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
-                    <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
-                </div>
-            </div>
-        </div>
+           <div class="flex items-center justify-between">
+    <div class="flex items-center space-x-4">
+        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+        <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
+        <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+    </div>
+    <div>
+        <a href="/jobs/create" class="text-gray-300 hover:text-white text-sm">Create Job</a>
+    </div>
+</div>
     </nav>
 
     <header class="bg-white shadow">
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,26 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => \App\Models\Job::with('employer')->paginate(3)]);
+    return view('jobs.index', ['jobs' => Job::with('employer')->latest()->paginate(3)]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/jobs/{id}', function ($id) {
@@ -18,7 +37,33 @@ Route::get('/jobs/{id}', function ($id) {
         abort(404, 'Job not found');
     }
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+    $job->delete();
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
